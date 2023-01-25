49f7123daddbc52107792361ab9af5d12b4a57ed
Nuralam30 <nuralam.hstu.930@gmail.com>
2023-01-25 17:47:02 +0600

    dropdown nav added in user

--- src/components/Navbar/NavMenu.js
+++ src/components/Navbar/NavMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,6 +9,8 @@ const NavMenu = () => {
     const userState = useSelector(state => state.userLoginReducer);
     const { currentUser } = userState;
 
+    const [dropdown, serDropdown] = useState(false)
+
     return (
         <div>
             <Navbar bg="light" variant="light" className='shadow-lg p-3 mb-5 bg-white rounded'>
@@ -16,7 +18,7 @@ const NavMenu = () => {
                 <Navbar.Brand href="/">Pizza Hut</Navbar.Brand>
                 <Nav className="ml-auto">
                     {
-                        currentUser ? <Nav.Link >{currentUser.name}</Nav.Link> : 
+                        currentUser ? <Nav.Link onClick={() => serDropdown( !dropdown)}>{currentUser.name}</Nav.Link> : 
                         <Nav.Link href="/login">Login</Nav.Link>
                     }
                     <Nav.Link href="/cart">Cart {cartState.cartItems.length}</Nav.Link>
